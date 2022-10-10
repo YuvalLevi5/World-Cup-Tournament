3758bc8e89d4a16f25f0be62423c5bad35e7bfe4
Yuval Vaknin Levi <yuvallevi0950@gmail.com>
2022-10-10 22:18:05 +0300

    top scorrer

---
+++ frontend/src/components/TopScorrer.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+import { useEffect } from 'react'
+
+const TopScorrer = () => {
+
+    const [winner, setWinner] = useState('')
+
+    useEffect(() => {
+        if (currentUser.winner) {
+            setWinner(currentUser.winner)
+        }
+    }, [])
+
+    const handleChange = (event) => {
+        const ans = event.target.value
+        setWinner(ans)
+    }
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        console.log(winner)
+    }
+    
+  return (
+    <div>
+    <form onSubmit={(e) => handleSubmit(e)} >
+
+        <h5>Enter your winner: </h5>
+        <input onChange={(e) => handleChange(e)} type="text" placeholder='World Cup Winner' value={winner} />
+        <button type='submit' className='btn' ></button>
+    </form>
+</div>
+  )
+}
+
+export default TopScorrer
