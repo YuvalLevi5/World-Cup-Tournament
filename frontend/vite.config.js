9e8a92f6694c75de745a9f4edbd6d27c007ee4aa
Yuval Vaknin Levi <yuvallevi0950@gmail.com>
2022-11-19 21:51:55 +0200

    build

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,32 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
-let faviconURL = './public/faveicon.svg'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: [faviconURL],
-      manifest: {
-        theme_color: '#ffffff',
-        icons: [
-          {
-            src: faviconURL,
-            sizes: '512x512',
-            type: 'image/svg+xml',
-            purpose: 'any maskable'
-          },
-          {
-            src: faviconURL,
-            sizes: '512x512',
-            type: 'image/png',
-          }
-        ]
-      },
-    })
   ],
   build: {
     outDir: '../backend/public',
